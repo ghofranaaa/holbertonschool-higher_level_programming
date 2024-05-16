038be574fc5cac315b455e8a4122ae9a1ef8ca54
ghofranaaa <smailikhaled841@gmail.com>
2024-05-16 15:45:13 +0100

    addition of unique numbers in a list

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     unique_numbrs = set()
-    sum = 0
     for i in my_list:
-        if i not in unique_numbrs:
-            sum += i
-            unique_numbrs.add(i)
-        return sum
+        unique_numbrs.add(i)
+    return sum(unique_numbrs)
