4d08932a6b58404554ae5996529862f09d4049e0
ghofranaaa <smailikhaled841@gmail.com>
2024-05-21 13:45:02 +0100

    first

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float, optional): The second number. Defaults to 98.
+
+    Returns:
+        int: The sum of a and b, casted to integers.
+
+    Raises:
+        TypeError: If either a or b is not an integer or float.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testfile("tests/0-add_integer.txt")
