1b8fae56588c8604fb69499c3ab14ce2adeaab33
ghofranaaa <smailikhaled841@gmail.com>
2024-05-29 01:19:45 +0100

    conversion of obj to json str

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module provides a function to convert a Python object \
+to its JSON string representation.
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string).
+
+    Args:
+        my_obj: The object to be serialized to JSON.
+
+    Returns:
+        str: The JSON representation of the object.
+    """
+    return json.dumps(my_obj)
