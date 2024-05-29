3e886a873ef7d743f2d5db48ac5e39b75bd3aeeb
ghofranaaa <smailikhaled841@gmail.com>
2024-05-29 01:03:46 +0100

    opening and reading a file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module provides a function to read the contents of a file.
+"""
+
+
+def read_file(filename=""):
+       """
+    Reads and prints the content of a file.
+
+    Args:
+        filename (str): The file to read from.
+    """
+    with open(filename. encoding="UTF8") as f:
+        print(f"{f.read()}", end="")
