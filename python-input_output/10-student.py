a3af67be85f96c02111ea4900fa723cba438a5f5
ghofranaaa <smailikhaled841@gmail.com>
2024-05-29 14:20:49 +0100

    filtering attributes

---
+++ python-input_output/10-student.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Module defines the Student class.
+"""
+
+
+class Student:
+    """
+    A class to represent a student.
+
+    Attributes:
+    ----------
+    first_name : str
+    last_name : str
+    age : int
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize the student instance with first name, last name, and age.
+
+        Parameters:
+        ----------
+        first_name : str
+            The first name of the student.
+        last_name : str
+            The last name of the student.
+        age : int
+            The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+        -------
+        dict
+            A dictionary representation of the student's attributes.
+        """
+        if isinstance(attrs, list):
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        return self.__dict__
