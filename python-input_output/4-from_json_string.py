61b929c5aa40ce01f3be79a57f48ca3dc460f561
ghofranaaa <smailikhaled841@gmail.com>
2024-05-29 01:23:06 +0100

    converting from json to py data structure

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module provides a function to deserialize a JSON string \
+to a Python object.
+"""
+
+import json
+
+def from_json_string(my_str):
+    """
+    Returns an object (Python data structure) represented by a JSON string.
+
+    Args:
+        my_str (str): The JSON string to be deserialized.
+
+    Returns:
+        object: The Python object represented by the JSON string.
+    """
+    return json.loads(my_str)
