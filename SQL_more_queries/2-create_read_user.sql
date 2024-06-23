c27193bc576fae579542446b424d6044271283d4
ghofranaaa <smailikhaled841@gmail.com>
2024-06-23 02:44:06 +0100

    only select privilege

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- creating a data base and a user, and giving the user only select privileges
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2 ;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT on *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+GRANT SELECT on hbtn_0d_2.* TO 'user_0d_2'@'localhost';
