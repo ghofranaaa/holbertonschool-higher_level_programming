3901209e6355ee3ac3c4abe2aa7fa5c2afaef0ef
ghofranaaa <smailikhaled841@gmail.com>
2024-06-23 03:33:43 +0100

    foreign key constraint

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- foreign key constraint
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0c_0;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities
 (id INT AUTO_INCREMENT PRIMARY KEY,
 state_id NOT NULL,
