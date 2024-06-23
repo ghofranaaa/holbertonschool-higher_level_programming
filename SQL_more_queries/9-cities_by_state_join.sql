d207aadace6c44563f55eb2439e398d4a364e28f
ghofranaaa <smailikhaled841@gmail.com>
2024-06-23 04:28:41 +0100

    joining two tables

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- selecting from two tables
 USE hbtn_0d_usa;
 SELECT cities.id, cities.name, states.name
-FROM cities
-JOIN states ON cities.state_id = states.id
+FROM cities, states
+WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
