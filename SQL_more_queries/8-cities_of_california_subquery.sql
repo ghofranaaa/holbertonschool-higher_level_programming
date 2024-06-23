ccf4e98a366f6570e03e49cf01dae41daeb601ae
ghofranaaa <smailikhaled841@gmail.com>
2024-06-23 03:58:35 +0100

    getting all cities of a specific state

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+-- listing all cities of a specific place
+USE hbtn_0d_usa;
+SELECT id, name
+FROM cities
+WHERE state_id = (
+	SELECT id
+	FROM states
+	WHERE name = 'California'
+)
+ORDER BY id ASC;
