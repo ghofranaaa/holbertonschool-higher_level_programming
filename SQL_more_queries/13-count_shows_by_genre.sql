519a20d13cee484c30c7fc7f159fa1a747c13127
ghofranaaa <smailikhaled841@gmail.com>
2024-06-23 15:40:45 +0100

    num of linked shows

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- counting num of shows that has the same genre
-SELECT genres.title AS genre, COUNT(tv_show_genres.show_id) AS num_of_shows
+SELECT genres.title AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM genres
 JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
 GROUP BY genres.title
-HAVING COUNT(tv_show_genres.show) > 0
-ORDER BY num_of_shows DESC;
+HAVING COUNT(tv_show_genres.show_id) > 0
+ORDER BY number_of_shows DESC;
