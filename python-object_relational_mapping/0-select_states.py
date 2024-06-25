09b5cb718201135085cb5df92a9a7c6118f80337
ghofranaaa <smailikhaled841@gmail.com>
2024-06-25 18:19:24 +0100

    interacting with mysql db

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
-import MySQLdb  # Import MySQLdb module to interact with MySQL database
-import sys  # Import sys module to handle command-line arguments
+"""
+This module lists all states from a database.
+
+Args:
+	username
+	password
+    database name
+"""
+import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
-    # Get MySQL username, password, and database name from command-line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database_name = sys.argv[3]
 
-# Connect to the MySQL database
-# MySQLdb.connect creates a connection object to interact with the database
-db = MySQLdb.connect(host="localhost", port=3306, user=username,
+	db = MySQLdb.connect(host="localhost", port=3306, user=username,
                      passwd=password, db=database_name)
 
-# Create a cursor object to execute SQL queries
-cursor = db.cursor()
+	cursor = db.cursor()
 
-# Execute SQL query to select all rows from the states table, ordered by id
-cursor.execute("SELECT * FROM states ORDER BY id ASC")
+	cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-# Fetch all the rows returned by the SQL query
-states = cursor.fetchall()
+	states = cursor.fetchall()
 
-# Iterate over each row in the result set and print it
-for state in states:
-    print(state)
+	for state in states:
+    	print(state)
 
-# Close the cursor to release the resources
-cursor.close()
+	cursor.close()
 
-# Close the database connection
-db.close()
+	db.close()
