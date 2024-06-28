2b59f7b32ec608e73588aa4ca325f4af27903089
ghofranaaa <smailikhaled841@gmail.com>
2024-06-28 11:27:07 +0100

    square

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module Square """
+
+class Square:
+    """ This is a Square class defined by geometric shap
+
+        Attributes:
+            size (int): Size of square
+    """
+    def __init__(self, size):
+        """
+        Initialize methode
+
+        Args:
+            size (int): size of square
+        """
+        self.__size = size
