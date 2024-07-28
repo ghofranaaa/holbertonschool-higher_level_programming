41c156174c340370a83cf9f8437410c24199c1b5
ghofranaaa <smailikhaled841@gmail.com>
2024-07-28 15:16:51 +0100

    square class

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module defines a Rectangle subclass Square.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """
+    Represents a square with a specified side length.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance with size.
+        """
+        super().__init__(size, size)
+        self.integer_validator('size', size)
+        self._size = size
+
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+        """
+        return self._size ** 2
