010ab38bd36105f962112817668f62fe7e5344ae
ghofranaaa <smailikhaled841@gmail.com>
2024-07-28 15:50:58 +0100

    iteration

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,14 @@
+class CountedIterator:
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.counter = 0
+
+    def get_count(self):
+        return self.counter
+
+    def __next__(self):
+        self.counter += 1
+        try:
+            return next(self.iterator)
+        except StopIteration:
+            raise StopIteration
