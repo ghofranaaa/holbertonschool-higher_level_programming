982d3b01f9fa39d31bbd899c7eb99149ff28ea8b
ghofranaaa <smailikhaled841@gmail.com>
2024-07-28 14:46:33 +0100

    this or that

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -16,6 +16,7 @@ def is_kind_of_class(obj, a_class):
     - a_class: The class to compare against.
 
     Returns:
-    - bool: True if obj is an instance of a_class or a subclass of a_class, False otherwise.
+    - bool: True if obj is an instance of a_class or a subclass of
+    a_class, False otherwise.
     """
     return isinstance(obj, a_class) or issubclass(type(obj), a_class)
