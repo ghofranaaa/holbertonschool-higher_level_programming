d846d9187ee0394a31117ff1ee8b6d84f7e8c07b
ghofranaaa <smailikhaled841@gmail.com>
2024-07-28 14:53:07 +0100

    base geo

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module provides a class `BaseGeometry`.
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry serves as a foundational class for geometric shapes.
+    It is intended to be subclassed by specific geometric shape classes.
+    """
+
+    def area(self):
+        """
+        Raises an Exception indicating that the area method is not implemented.
+
+        Raises:
+            Exception: An exception with the message "area() is not implemented".
+        """
+        raise Exception("area() is not implemented")
