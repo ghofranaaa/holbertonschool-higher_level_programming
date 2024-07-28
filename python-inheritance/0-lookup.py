7302e07f5c2677bcd762d467884a23721695ea14
ghofranaaa <smailikhaled841@gmail.com>
2024-07-28 14:27:32 +0100

    list of attrb

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""This module defines a utility function for looking up attributes and methods of an object.
+"""
+
+def lookup(obj):
+    """Returns a list of attributes and methods of the given object."""
+    return list(dir(obj))
