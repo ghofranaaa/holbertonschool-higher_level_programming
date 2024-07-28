79d5a99192bf165b00bc8a254c9a1c296cf39716
ghofranaaa <smailikhaled841@gmail.com>
2024-07-28 15:44:27 +0100

    duck typing

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,49 +1,36 @@
-from math import pi
 from abc import ABC, abstractmethod
+import math
 
 class Shape(ABC):
     @abstractmethod
     def area(self):
         pass
-
+    
     @abstractmethod
     def perimeter(self):
         pass
 
 class Circle(Shape):
-    """Class representing a circle."""
-
     def __init__(self, radius):
-        if radius < 0:
-            raise ValueError("Radius must be non-negative")
         self.radius = radius
-
+    
     def area(self):
-        return pi * self.radius ** 2
-
+        return math.pi * self.radius ** 2
+    
     def perimeter(self):
-        return 2 * pi * self.radius
+        return 2 * math.pi * self.radius
 
 class Rectangle(Shape):
-    """Class representing a rectangle."""
-
     def __init__(self, width, height):
         self.width = width
         self.height = height
-
+    
     def area(self):
         return self.width * self.height
-
+    
     def perimeter(self):
         return 2 * (self.width + self.height)
 
 def shape_info(shape):
-    print("Area:", shape.area())
-    print("Perimeter:", shape.perimeter())
-
-if __name__ == "__main__":
-    circle = Circle(radius=5)
-    rectangle = Rectangle(width=4, height=7)
-
-    shape_info(circle)
-    shape_info(rectangle)
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
