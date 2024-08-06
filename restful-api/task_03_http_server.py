47fc3cf8d854bc5ce4bd4a37c6f487772b0466b0
ghofranaaa <smailikhaled841@gmail.com>
2024-08-06 14:39:38 +0100

    fixed

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -39,7 +39,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"Not Found")
+            self.wfile.write(b"404 Not Found")
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
     server_address = ("localhost", port)
