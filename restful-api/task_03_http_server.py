eb15d50f1aa937d406838de989ad45428d475e12
ghofranaaa <smailikhaled841@gmail.com>
2024-08-06 14:38:05 +0100

    fixed

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -39,7 +39,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"Endpoint not found")  # Custom message for 404 error
+            self.wfile.write(b"Not Found")
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
     server_address = ("localhost", port)
