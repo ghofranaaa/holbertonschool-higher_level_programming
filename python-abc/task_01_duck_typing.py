be33d51393247e642759c4228dafcf9483688a04
ghofranaaa <smailikhaled841@gmail.com>
2024-08-07 15:53:32 +0100

    fix

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
-from math import pi
+import math
 
 class Shape(ABC):
+
     @abstractmethod
     def area(self):
         pass
@@ -9,18 +10,17 @@ class Shape(ABC):
     @abstractmethod
     def perimeter(self):
         pass
+
 
 class Circle(Shape):
     def __init__(self, radius):
-        if radius < 0:
-            raise ValueError("Radius cannot be negative")
         self.radius = radius
 
     def area(self):
-        return pi * self.radius ** 2
+        return math.pi * self.radius ** 2
 
     def perimeter(self):
-        return 2 * pi * self.radius
+        return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
@@ -34,13 +34,11 @@ class Rectangle(Shape):
     def perimeter(self):
         return 2 * (self.width + self.height)
 
+
 def shape_info(shape):
     print(f"Area: {shape.area()}")
     print(f"Perimeter: {shape.perimeter()}")
 
-if __name__ == "__main__":
-    circle = Circle(radius=5)
-    rectangle = Rectangle(width=4, height=7)
 
     shape_info(circle)
     shape_info(rectangle)
